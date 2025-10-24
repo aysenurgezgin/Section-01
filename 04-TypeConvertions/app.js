f196a4d1d1bac92b59919647d91fbf57570b4867
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-10-24 16:23:55 +0300

    continue reminder

---
+++ 04-TypeConvertions/app.js
@@ -0,0 +1,54 @@
+// 1) toString
+
+// let isActive = true;
+// console.log(isActive, typeof isActive);
+// let isActiveString =String(isActive);
+// console.log(isActiveString, typeof isActiveString);
+
+// let price =37800;
+// console.log(price, typeof price);
+// let priceString =String(price);
+// console.log(priceString, typeof priceString);
+
+// let firstName ="aylin";
+// let lastName="sezer";
+// let fullName =firstName +" "+lastName;
+// console.log(fullName);
+
+// console.log("ali"+"veli");
+// console.log(4+9);
+// console.log("ali"+9);
+// console.log("10"+9);
+// console.log(10+55+"9");
+
+
+// 2)Number
+
+// console.log("6"/"3");
+// console.log(Number("80"));
+// console.log(Number("sezer")); 
+// console.log(Number("85+9"));
+// let value1=null;
+// let value2=undefined;
+// let value3=true;
+// let value4=false;
+
+// console.log(Number(value1));
+// console.log(Number(value2));
+// console.log(Number(value3));
+// console.log(Number(value4));
+
+// 3)Boolean
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(1-598));
+// console.log(Boolean("ayşe"));
+// console.log(Boolean("JavaScript"));
+// console.log(Boolean(""));
+// console.log(Boolean("."));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(Number("1")));
+//var yok boolean
+
