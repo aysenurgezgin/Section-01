3dcc3f64c466a7cea53c02aca9adfa384e3628d0
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-10-25 15:17:41 +0300

    strict mode

---
+++ 11-Arrays/app.js
@@ -0,0 +1,15 @@
+// let products=[];
+// products[0]="samsung 22";
+// products[1]="samsung 22s";
+// products[20]="altus";
+// console.log(products);
+// console.log(products[15]);
+
+let products=["vestel","Iphone","togg","samsung","devrim"];
+console.log(products);
+
+let prices =[45000, 50000,1800000,25000,950000];
+console.log(prices);
+
+let student5=["aylin","sezer",30,true]
+console.log(student5);
