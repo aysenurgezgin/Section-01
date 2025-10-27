709883ed353384b4a98729b5e2bc309785366a09
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-10-27 13:31:30 +0300

    update

--- 14-Functions/app.js
+++ 14-Functions/app.js
@@ -1,34 +1,19 @@
 "use strict"
-function selamVer(){
-    console.log("hi");
-}
-selamVer();
+// function selamVer(){
+//     console.log("hi");
+// }
+// selamVer();
 
- function today() {
-    let result = new Date();
+//  function today() {
+//     let result = new Date();
+//     return result;
+// }
+
+//soru:çalıştırıldığında "yazı""tura"şeklinde sonuç veren kod;
+
+function yaziTura(){
+    let random=Math.random();
+    let result=random>=0.5 ? "yazı" :"tura";
     return result;
 }
-
-let bugun = today();
-console.log(bugun);
-
-let names = ["Ahmet", "Mehmet", "Yeşim"];
-let departments = ['İnşaat', 'Yazılım', 'Mimarlık'];
-let result = [];
-let random = 3;
-for (let i = 0; i < names.length; i++) {
-    while (random > 2) {
-        random = parseInt(Math.random() * 10);
-    }
-    result.push({ name: names[i], department: departments[random] });
-    random = 3;
-}
-console.log(result);
-
-let random = Math.random();
-console.log(random);
-console.log(random * 5);
-console.log(Math.floor(random));
-
-3-10 arasında rasgele sayı üretmek
-let random = Math.floor(Math.random() * 7 + 3);
+console.log(yaziTura());
